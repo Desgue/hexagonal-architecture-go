f649d6b378170bbfcc11213ef60e80a000efb2c9
Desgue <guedesgomero@gmail.com>
2023-10-12 01:59:19 +0100

    minor refactor in dynamo struct and in repository testing

--- internal/repositories/usersrepo/dyanmodb.go
+++ internal/repositories/usersrepo/dyanmodb.go
@@ -1,12 +1,34 @@
 package usersrepo
 
-import "github.com/Desgue/hexagonal-architecture-go-example/internal/core/domain"
+import (
+	"github.com/Desgue/hexagonal-architecture-go-example/internal/core/domain"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/dynamodb"
+)
 
 type dynamoRepository struct {
+	client    *dynamodb.DynamoDB
+	tableName string
 }
 
-func NewDynamoRepository() *dynamoRepository {
-	return &dynamoRepository{}
+func configDB() *dynamodb.DynamoDB {
+	sess := session.Must(session.NewSessionWithOptions(session.Options{
+		Config: aws.Config{
+			Region:   aws.String("us-east-1"),
+			Endpoint: aws.String("http://localhost:8080"),
+		},
+		Profile: "default",
+	}))
+	client := dynamodb.New(sess)
+	return client
+
+}
+func NewDynamoRepository(tableName string) *dynamoRepository {
+	return &dynamoRepository{
+		client:    configDB(),
+		tableName: tableName,
+	}
 }
 
 // By defining these two methods we implement the UserRepository interface from ports
