c569c3db95e811fbf8bae0593ea7ec74133a0b3e
Desgue <guedesgomero@gmail.com>
2023-10-19 02:08:03 +0100

    Refactor code structure and end repository implementation and testing

--- internal/repositories/usersrepo/dynamodb_test.go
+++ internal/repositories/usersrepo/dynamodb_test.go
@@ -12,6 +12,74 @@ import (
 	"github.com/testcontainers/testcontainers-go/modules/localstack"
 )
 
+func TestInsert(t *testing.T) {
+	ctx := context.Background()
+	localStackCont, err := prepareContainer(ctx)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer func() {
+		if err := localStackCont.Terminate(ctx); err != nil {
+			panic(err)
+		}
+	}()
+	repo := NewDynamoRepository(localStackCont.URI)
+	if err := repo.createTable(); err != nil {
+		t.Fatal(err)
+	}
+	newUser := domain.User{
+		Id:   "1",
+		Name: "Tester",
+	}
+	gotUser, err := repo.Insert(newUser)
+	if err != nil {
+		t.Errorf("Got error: %s when inserting new user", err)
+	}
+
+	if !reflect.DeepEqual(gotUser.Id, "1") {
+		t.Errorf("Got %v want %v", gotUser.Id, "1")
+	}
+	if !reflect.DeepEqual(gotUser.Name, "Tester") {
+		t.Errorf("Got %v want %v", gotUser.Name, "Tester")
+	}
+
+}
+func TestFindById(t *testing.T) {
+	ctx := context.Background()
+	localStackCont, err := prepareContainer(ctx)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer func() {
+		if err := localStackCont.Terminate(ctx); err != nil {
+			panic(err)
+		}
+	}()
+	repo := NewDynamoRepository(localStackCont.URI)
+	if err := repo.createTable(); err != nil {
+		t.Fatal(err)
+	}
+	newUser := domain.User{
+		Id:   "1",
+		Name: "Tester",
+	}
+	_, err = repo.Insert(newUser)
+	if err != nil {
+		t.Errorf("Got error inserting user: %s", err)
+	}
+	gotUser, err := repo.FindById(newUser.Id)
+	if err != nil {
+		t.Errorf("Got error searching user: %s", err)
+	}
+	if !reflect.DeepEqual(gotUser.Id, "1") {
+		t.Errorf("Got %v want %v", gotUser.Id, "1")
+	}
+	if !reflect.DeepEqual(gotUser.Name, "Tester") {
+		t.Errorf("Got %v want %v", gotUser.Name, "Tester")
+	}
+
+}
+
 type container struct {
 	*localstack.LocalStackContainer
 	URI string
@@ -36,72 +104,4 @@ func prepareContainer(ctx context.Context) (*container, error) {
 	}
 	URI := fmt.Sprintf("http://%s:%d", host, mappedPort.Int())
 	return &container{LocalStackContainer: cont, URI: URI}, nil
-}
-
-func TestInsert(t *testing.T) {
-	ctx := context.Background()
-	localStack, err := prepareContainer(ctx)
-	if err != nil {
-		t.Fatal(err)
-	}
-	defer func() {
-		if err := localStack.Terminate(ctx); err != nil {
-			panic(err)
-		}
-	}()
-	repo := NewDynamoRepository(localStack.URI)
-	if err := repo.createTable(); err != nil {
-		t.Fatal(err)
-	}
-	newUser := domain.User{
-		Id:   "1",
-		Name: "Tester",
-	}
-	gotUser, err := repo.Insert(newUser)
-	if err != nil {
-		t.Errorf("Got error: %s when inserting new user", err)
-	}
-
-	if !reflect.DeepEqual(gotUser.Id, "1") {
-		t.Errorf("Got %v want %v", gotUser.Id, "1")
-	}
-	if !reflect.DeepEqual(gotUser.Name, "Tester") {
-		t.Errorf("Got %v want %v", gotUser.Name, "Tester")
-	}
-
-}
-func TestFindById(t *testing.T) {
-	ctx := context.Background()
-	localStack, err := prepareContainer(ctx)
-	if err != nil {
-		t.Fatal(err)
-	}
-	defer func() {
-		if err := localStack.Terminate(ctx); err != nil {
-			panic(err)
-		}
-	}()
-	repo := NewDynamoRepository(localStack.URI)
-	if err := repo.createTable(); err != nil {
-		t.Fatal(err)
-	}
-	newUser := domain.User{
-		Id:   "1",
-		Name: "Tester",
-	}
-	_, err = repo.Insert(newUser)
-	if err != nil {
-		t.Errorf("Got error inserting user: %s", err)
-	}
-	gotUser, err := repo.FindById(newUser.Id)
-	if err != nil {
-		t.Errorf("Got error searching user: %s", err)
-	}
-	if !reflect.DeepEqual(gotUser.Id, "1") {
-		t.Errorf("Got %v want %v", gotUser.Id, "1")
-	}
-	if !reflect.DeepEqual(gotUser.Name, "Tester") {
-		t.Errorf("Got %v want %v", gotUser.Name, "Tester")
-	}
-
 }
