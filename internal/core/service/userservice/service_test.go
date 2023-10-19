c636d49d1593ee92e534a7bbb19241b8b8744c07
Desgue <guedesgomero@gmail.com>
2023-10-19 03:04:10 +0100

    Add unit test for Create method and fake in memory repository for testing

--- internal/core/service/userservice/service_test.go
+++ internal/core/service/userservice/service_test.go
@@ -1,9 +1,31 @@
 package userservice
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+
+	"github.com/Desgue/hexagonal-architecture-go-example/internal/core/domain"
+	"github.com/Desgue/hexagonal-architecture-go-example/internal/repositories/usersrepo"
+)
 
 func TestCreate(t *testing.T) {
+	user := domain.User{
+		Name: "Tester",
+	}
+	repo := usersrepo.NewFakeRepository(map[string]domain.User{})
+	service := NewUserService(repo)
 
+	got, err := service.Create(user)
+	if err != nil {
+		t.Error(err)
+	}
+
+	if reflect.DeepEqual(got.Id, "") {
+		t.Error("Error as Id was not successfully created")
+	}
+	if !reflect.DeepEqual(got.Name, user.Name) {
+		t.Errorf("Got %s want %s", got.Name, user.Name)
+	}
 }
 
 func TestGetAll(t *testing.T) {
